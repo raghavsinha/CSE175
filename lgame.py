a4a1dc43a7cf4eafab3b220fb20d32cdc52b1b74
Raghav Sinha <raghav@sinhas.net>
2024-11-28 00:05:00 -0800

    lgame isValidMoveFormat and isValidMove initial code complete

--- lgame.py
+++ lgame.py
@@ -1,3 +1,5 @@
+import re
+
 # Key:
 # 0 - blank
 # 1 - blue
@@ -106,36 +108,65 @@ def getSuccessors(gameState, agent):
 
         validDotPositions = getLegalDotPos(nextGameState)
         
-        for d in validDot1Positions: # moving dot 1
-            nextGameState1 = nextGameState.copy()
-            nextGameState1[d[0]][d[1]] = DOT
-            successorStates.add(nextGameState1)
-        for d in validDot2Positions: # moving dot 2
-            nextGameState1 = nextGameState.copy()
-            nextGameState1[d[0]][d[1]] = DOT
-            successorStates.add(nextGameState1)
+
 
     return successorStates
 
 
-def isValidMove(gameState, agent, move):
-    #if dot moved, len(move) == 13, otherwise len(move) = 5
-    xMove = move[0], yMove = move[2], orientation = move[4]
-    lCoords = [(xMove, yMove)]
-    if(orientation == 'E'):
-        lCoords.add([(xMove, yMove + 1), (xMove - 1, yMove), (xMove - 2, yMove)])
-    elif(orientation == 'S'):
-        lCoords.add([(xMove + 1, yMove), (xMove, yMove + 1), (xMove, yMove + 2)])
-    elif(orientation == 'W'):
-        lCoords.add([(xMove, yMove - 1), (xMove + 1, yMove), (xMove + 2, yMove)])
-    elif(orientation == 'N'):
-        lCoords.add([(xMove - 1, yMove), (xMove, yMove - 1), (xMove, yMove - 2)])
+def isValidMoveFormat(move):
+    # Define the format using a regular expression
+    pattern = r"^\d \d [A-Za-z] \d \d \d \d$"
+    if not re.match(pattern, move):
+        return False
+    
+    nums = [0, 2, 6, 8, 10, 12]
+    for n in nums:
+        if(int(move[n]) not in range(1, 5)):
+            return False
 
-    legalLCoords = True
-    for c in lCoords:
-        if(i < 0 or i > 3 or j < 0 or j > 3):
-            legalLCoords = False
-            break
-        if((gameState[i][j] != agent and gameState[i][j] != BLANK)):
-            legalLCoords = False
-            break
+    validOrientations = ['E', 'S', 'W', 'N']
+    if(move[4] not in validOrientations):
+        return False
+
+    return True
+
+
+def isValidMove(gameState, agent, move):
+    if(isValidMoveFormat(move)):
+        #if dot moved, len(move) == 13, otherwise len(move) = 5
+        xMove = int(move[0]) - 1, yMove = int(move[2]) - 1, orientation = move[4]
+        lCoords = [(xMove, yMove)]
+        if(orientation == 'E'):
+            lCoords.add([(xMove, yMove + 1), (xMove - 1, yMove), (xMove - 2, yMove)])
+        elif(orientation == 'S'):
+            lCoords.add([(xMove + 1, yMove), (xMove, yMove + 1), (xMove, yMove + 2)])
+        elif(orientation == 'W'):
+            lCoords.add([(xMove, yMove - 1), (xMove + 1, yMove), (xMove + 2, yMove)])
+        elif(orientation == 'N'):
+            lCoords.add([(xMove - 1, yMove), (xMove, yMove - 1), (xMove, yMove - 2)])
+
+        legalLCoords = True
+        for c in lCoords:
+            if(c[0] < 0 or c[0] > 3 or c[1] < 0 or c[1] > 3):
+                legalLCoords = False
+                break
+            if((gameState[c[0]][c[1]] != agent and gameState[c[0]][c[1]] != BLANK)):
+                legalLCoords = False
+                break
+
+        if(not legalLCoords):
+            return False
+
+        if(len(move) > 5):
+            dotInit = (int(move[6]) - 1, int(move[8]) - 1)
+            dotFinal = (int(move[10]) - 1, int(move[12]) - 1)
+            if(dotInit == dotFinal):
+                return False
+            if(gameState[dotInit[0]][dotInit[1]] != DOT):
+                return False
+            if(gameState[dotFinal[0]][dotFinal[1]] == DOT):
+                return False
+    else:
+        return False
+    
+    return True
